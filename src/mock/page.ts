99e3113b9c9a7b52c2c560a4847477f687ec2b79
czf <122385930@qq.com>
2022-07-24 00:16:07 +0800

    修改mock/page.ts

--- src/mock/page.ts
+++ src/mock/page.ts
@@ -3,17 +3,38 @@ import content from './content'
 export default {
   tilte: 'xxxx',
   icon: '/favicon.svg',
+  lang: 'en',
   meta: {
     keywords: '',
     description: '',
   },
-  lang: 'en',
+  content,
   path: '/custom/demo',
   query: {
     id: 1,
   },
-  version: '0.0.0',
-  template: '圣杯布局',
+  template: '圣杯布局-1',
   timezone: 'China Standard Time',
-  content,
+  mode: 0, // 空白状态：0  查看状态: 1 编辑状态：2
+  published: 0, // 预览 发布
+  version: '<% major %>.<% minor %>.<% modify %>', // 随发布变更
+  logs: [
+    {
+      info: '', // 信息
+      timestamp: '2022-07-23 23:58:55',
+      version: '<% version %>'
+    }
+  ], // 日志信息, 追加时间，可以按时间轴方式或按天进行汇总显示
+  errors: [
+    {
+      name: 'XxxError',
+      message: 'xxxxxxx',
+    }
+  ], // 错误信息
+  operations: [
+    {
+      name: 'xxx',
+      message: 'yyyy'
+    }
+  ], // 操作记录
 }
