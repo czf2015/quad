2ee7b65cca1c54d32e61ac96f1e9b56d89e53f6a
czf <122385930@qq.com>
2022-07-23 10:32:09 +0800

    添加useCRUD：用于处理增删改查逻辑

---
+++ src/hooks/useCRUD.ts
@@ -0,0 +1,71 @@
+// @ts-nocheck
+import { useState } from 'react'
+
+const delayPromise = (fn, duration) => (...params) => {
+  return new Promise(async (resolve, reject) => {
+    setTimeout(() => {
+      try {
+        resolve(await fn(...params))
+      } catch (e) {
+        reject(e)
+      }
+    }, duration)
+  })
+}
+
+export const useCRUD = ($query, initials) => {
+  const [loading, setLoading] = useState(false)
+  const [queryParams, setQueryParams] = useState(initials?.queryParams)
+  const [data, setData] = useState(initials?.data)
+  const [error, setError] = useState()
+
+  const query = ({ params = queryParams, limit = 1 } = {}) => {
+    setQueryParams(params)
+    setLoading(true)
+    return $query(params)
+      .then(res => {
+        setData(res.data)
+        return res
+      })
+      .catch(error => {
+        limit--
+        if (limit) {
+          return query({ params, limit })
+        }
+        setError(error)
+        return Promise.reject(error)
+      })
+      .finally(_ => {
+        setLoading(false)
+      })
+  }
+
+  const crud = (request) => {
+    if (request) {
+      return ({ data, isRefresh = false, delay = false }) => {
+        setLoading(true)
+        return request(data)
+          .then(res => {
+            if (isRefresh) {
+              if (delay) {
+                return delayPromise(query, delay)()
+              }
+              return query()
+            }
+            return res
+          })
+          .catch(error => {
+            setError(error)
+            return Promise.reject(error)
+          })
+          .finally(_ => {
+            setLoading(false)
+          })
+      }
+    }
+
+    return query
+  }
+
+  return { loading, queryParams, data, error, query, crud }
+}
