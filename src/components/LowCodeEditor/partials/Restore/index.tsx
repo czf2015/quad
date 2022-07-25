827844690e06e91406da7daf8fb3b3b64a0f8f6e
caizhangfeng <122385930@qq.com>
2022-07-25 10:16:47 +0800

    完善交互

--- src/components/LowCodeEditor/partials/Restore/index.tsx
+++ src/components/LowCodeEditor/partials/Restore/index.tsx
@@ -4,14 +4,14 @@ import Button from '@/components/Button'
 import { DeleteOutlined, UndoOutlined, RedoOutlined, CameraOutlined } from '@ant-design/icons'
 import styles from './index.module.less'
 
-export const Restore = ({ mode = 0, restart, undo, redo, stage }) => {
+export const Restore = ({ mode = 0, prev = 0, next = 0, restart, undo, redo, stage }) => {
   const isEditMode = mode == 2
 
   return isEditMode ? (
     <div className={styles.restore}>
       <Button tip="还原" onClick={restart} type="text" icon={<DeleteOutlined />} />
-      <Button tip="撤销" onClick={undo} type="text" icon={<UndoOutlined />} />
-      <Button tip="重做" onClick={redo} type="text" icon={<RedoOutlined />} />
+      <Button tip="撤销" disabled={prev == 0} onClick={undo} type="text" icon={<UndoOutlined />} />
+      <Button tip="重做" disabled={next == 0} onClick={redo} type="text" icon={<RedoOutlined />} />
       <Button tip="暂存" onClick={stage} type="text" icon={<CameraOutlined />} />
     </div>
   ) : null
