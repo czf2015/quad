5007c01b627a231aff552779e4d6853385ad055f
caizhangfeng <122385930@qq.com>
2022-07-25 18:19:19 +0800

    添加拖拽逻辑

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Button, Tooltip } from 'antd'
 import styles from './index.module.less'
 
-export default ({ title = 'Button', tip, disabled = false, onClick = () => console.log(title), type = 'primary', icon }) => (
+export default ({ title, tip, disabled = false, onClick = () => console.log(title), type = 'primary', icon }) => (
   <Tooltip title={tip} placement="top" color='blue' >
     <Button className={styles.button} type={type} disabled={disabled} onClick={onClick} icon={icon}>{title}</Button>
   </Tooltip>
