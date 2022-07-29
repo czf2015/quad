6411a978fb9ddc643dcc5e9d21a47349021d6060
caizhangfeng <122385930@qq.com>
2022-07-29 10:52:46 +0800

    完善数据结构

---
+++ src/types/DataTable.d.ts
@@ -0,0 +1,116 @@
+import { CSSProperties } from "react";
+
+enum enumFormItemType {
+  TEXT,
+  NUMBER,
+  CHECK,
+  SINGLE,
+  MULTIPLE,
+  DATE,
+  ATTACHMENT,
+  RATING,
+  TAGS,
+  KIND,
+}
+
+type validatorType = (val: any) => boolean;
+
+type optionType = {
+  label: string;
+  value: number | string;
+};
+
+type dateType = string;
+type tagType = {
+  label: string;
+  value: number | string;
+};
+type kindType = {
+  label: string;
+  value: number | string;
+};
+
+interface IFormItem {
+  type: enumFormItemType;
+  field: string;
+  label?: string;
+  value?: any;
+  required?: boolean;
+  validator?: validatorType;
+  disabled?: boolean;
+  style: CSSProperties;
+}
+
+interface ITextFormItem extends IFormItem {
+  type: enumFormItemType.TEXT;
+  value: string;
+  minRows?: number;
+  maxRows?: number;
+  lineWidth?: number;
+  lineHeight?: number;
+  maxLength?: munber;
+}
+
+interface INumberFormItem extends IFormItem {
+  type: enumFormItemType.NUMBER;
+  value: number;
+  min?: number;
+  max?: number;
+  step?: number;
+}
+
+interface ICheckFormItem extends IFormItem {
+  type: enumFormItemType.CHECK;
+  value: boolean;
+  min?: number;
+  max?: number;
+  step?: number;
+}
+
+interface ISingleFormItem extends IFormItem {
+  type: enumFormItemType.SINGLE;
+  vaue: number | string;
+  options: optionType[];
+}
+
+interface IMultipleFormItem extends IFormItem {
+  type: enumFormItemType.MULTIPLE;
+  value: (number | string)[];
+  options: optionType[];
+}
+
+interface IDateFormItem extends IFormItem {
+  type: enumFormItemType.DATE;
+  value: dateType;
+}
+
+interface ILimit {
+  length: number;
+  total: number;
+}
+
+interface IAttachmentFormItem extends IFormItem {
+  type: enumFormItemType.ATTACHMENT;
+  value: attachmentType;
+  limit?: ILimit;
+}
+
+interface IRatingFormItem extends IFormItem {
+  type: enumFormItemType.RATING;
+  value: number;
+  min?: number;
+  max?: number;
+}
+
+interface ITagsFormItem extends IFormItem {
+  type: enumFormItemType.TAGS;
+  value: (number | string)[];
+  options: tagType[];
+}
+
+interface IKindFormItem extends IFormItem {
+  type: enumFormItemType.KIND;
+  value: number | string;
+  options: kindType[];
+}
+
