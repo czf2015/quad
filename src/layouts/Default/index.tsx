a2db1f3a89c6450a653a1d3e4cb13ec4a45888ec
czf <122385930@qq.com>
2022-08-01 22:21:31 +0800

    样式处理

--- src/layouts/Default/index.tsx
+++ src/layouts/Default/index.tsx
@@ -4,10 +4,10 @@ import { useResponsive } from '@/hooks'
 import styles from './index.module.less'
 
 export default ({ slots: { header, main, footer }, zoom }) => {
-  // const ref = useResponsive({ width: 1920, height: 1080 }, false)
+  const ref = useResponsive({ width: 1920, height: 1080 }, false)
 
   return (
-    <div className={styles.layout}/*  ref={ref} */>
+    <div className={styles.layout} ref={ref}>
       <div className={styles.header}>{header}</div>
       <div className={styles.main}>
         <div className={styles.left}>{main.left}</div>
