b7d90bca250c0b969dd6cdf71b0d8d3544f5d6d3
czf <122385930@qq.com>
2022-08-13 12:03:08 +0800

    调整样式

--- src/layouts/Default/index.tsx
+++ src/layouts/Default/index.tsx
@@ -1,18 +1,25 @@
 // @ts-nocheck
 import React from 'react'
-import { useResponsive } from '@/hooks'
+import { /* useResponsive,  */useToggle } from '@/hooks'
+import { LeftOutlined } from '@ant-design/icons'
 import styles from './index.module.less'
 
 export default ({ slots: { header, main, footer }, zoom, isPreview = true }) => {
   // const ref = useResponsive({ width: 1920, height: 1080 }, false)
+  const [leftPanelCollapsed, toggleLeftPanelCollapsed] = useToggle(false)
 
   return (
-    <div className={styles.layout}/*  ref={ref} */>
+    <div className={`${styles.layout} ${isPreview ? styles.preview : ''} ${leftPanelCollapsed ? styles.collapsed : ''}`}/*  ref={ref} */>
       <div className={styles.header}>{header}</div>
       <div className={styles.main}>
-        <div className={styles.left} style={{ display: isPreview ? 'none' : 'block' }}>{main.left}</div>
+        <div className={styles.left}>{main.left}</div>
         <div className={styles.content} /* style={{ transform: `scale(${zoom})`, width: 1440 / zoom, height: `calc(100% / ${zoom})` }} */>{main.content}</div>
-        <div className={styles.right} style={{ display: isPreview ? 'none' : 'block' }}>{main.right}</div>
+        <div className={styles.right}>
+          <span className={`${styles.collapse_btn} quad-circle`} onClick={toggleLeftPanelCollapsed}>
+            <LeftOutlined rotate={leftPanelCollapsed ? 180 : 0} />
+          </span>
+          {main.right}
+        </div>
       </div>
       {/* <div className={styles.footer}>{footer}</div> */}
     </div>
