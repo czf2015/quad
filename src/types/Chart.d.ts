596975f1f4c86d32d571f9c1f3536ed469884071
caizhangfeng <122385930@qq.com>
2022-08-19 19:13:10 +0800

    完善图表配置数据结构

--- src/types/Chart.d.ts
+++ src/types/Chart.d.ts
@@ -1,35 +1,73 @@
+type colorType = string;
+type offsetType = number;
+
+type colorStopType = {
+  offset: offsetType;
+  color: colorType; // 0% 处的颜色
+};
+
+type linearGradientType = {
+  type: "linear";
+  x: offsetType;
+  y: offsetType;
+  x2: offsetType;
+  y2: offsetType;
+  colorStops: colorStopType[];
+};
+
+type radialGradientType = {
+  type: "radial";
+  x: offsetType;
+  y: offsetType;
+  r: offsetType;
+  colorStops: colorStopType[];
+};
+
 interface ChartOption {
-  wrapper: {
-    type: "card";
-  };
   type: "BarChart" | "PieChart";
-  layout: "horizontal" | "vertical";
-  // subtype: 'base' |
-  group: "year";
-  stack: "region";
-  numeric: 0; // -数值 -百分比
-  scale: 0; // 坐标比例尺
+  layout: "horizontal" | "vertical"; // 水平或垂直 TB BT LR RL
+  group: string;
+  stack: string;
+  percentage: boolean; // 是否百分比值
   dimensions: [
     {
-      type: 0;
-      label: "维度(X轴)";
-      value: "name";
-      unit: "天";
+      label: string;
+      field: string;
+      sort: 0 | 1 | -1; // 原序 正序 倒序
+      scale: 0; //
+      unit: string;
     },
     {
-      type: 1;
-      label: "数量(Y轴)";
-      value: "number";
-      unit: "个";
+      label: string;
+      field: string;
+      sort: 0 | 1 | -1; // 原序 正序 倒序
+      scale: 0; //
+      unit: string;
     }
   ];
-  refer: {};
+  coordinate: {
+    type: "cartesian2d" | "polar" = "cartesian2d";
+    xAxis: {
+      index: number = 0;
+      unique?: string;
+    };
+    yAxis: {
+      index: number = 1;
+      unique?: string;
+    };
+  };
   animation: {
     enable: boolean;
     duration: number;
-  }
-  coordinate: '',
-  darkMode: boolean;
-  color: [];
-  
+  };
+  colors: (colorType | linearGradientType | radialGradientType)[];
+  title: string;
+  style: {
+    // width: number;
+    // height: number;
+    // margin: number;
+    // padding: number;
+    // border:
+    // borderRadius
+  };
 }
