675d41d9f06ef1a39cb5d4123e11358296470c6c
caizhangfeng <122385930@qq.com>
2022-08-19 18:18:49 +0800

    调整发布页面样式

--- src/pages/custom-page/index.tsx
+++ src/pages/custom-page/index.tsx
@@ -4,7 +4,7 @@ import { useEntities, useResponsive } from '@/hooks'
 import { PageService } from '@/services'
 
 export default () => {
-  const ref = useResponsive({ width: 1920, height: 1080 }, false)
+  const ref = useResponsive({ width: 1440, height: 1080 }, false)
 
   const { entities, ...attrs } = useEntities()
 
@@ -12,13 +12,12 @@ export default () => {
   useEffect(() => {
     PageService.getPublishPage({ path: window.location.pathname })
       .then((res) => {
-        // debugger
         setPage(res.data)
       })
   }, [])
 
   return (
-    <div ref={ref}>
+    <div ref={ref} style={{ width: page.width, height: page.height }}>
       <DisplayViewer entities={page.content} width={page.width} height={page.height}  {...attrs} />
     </div>
   )
