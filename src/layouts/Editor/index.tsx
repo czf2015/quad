ac71df9c48262ac2c9512ba3eeacf2101ea88044
czf <122385930@qq.com>
2022-08-27 11:50:17 +0800

    优化交互

--- src/layouts/Editor/index.tsx
+++ src/layouts/Editor/index.tsx
@@ -1,25 +1,23 @@
 // @ts-nocheck
 import React from 'react'
-import { /* useResponsive,  */useToggle } from '@/hooks'
-import { RightOutlined } from '@ant-design/icons'
+import { useToggle } from '@/hooks'
+import { LeftOutlined } from '@ant-design/icons'
 import styles from './index.module.less'
 
 export default ({ slots: { header, main, footer }, zoom, isPreview = true, exit }) => {
-  // const ref = useResponsive({ width: 1920, height: 1080 }, false)
-  const [rightPanelCollapsed, toggleRightPanelCollapsed] = useToggle(true)
+  const [panelCollapsed, togglePanelCollapsed] = useToggle(false)
 
   return (
-    <div className={`${styles.layout} ${isPreview ? styles.preview : ''} ${rightPanelCollapsed ? styles.collapsed : ''}`}/*  ref={ref} */>
+    <div className={`${styles.layout} ${isPreview ? styles.preview : ''} ${panelCollapsed ? styles.collapsed : ''}`}>
       <div className={styles.header}>{header}</div>
       <div className={styles.main}>
-        <div className={styles.left}>{main.left}</div>
-        <div className={styles.content} /* style={{ transform: `scale(${zoom})`, width: 1440 / zoom, height: `calc(100% / ${zoom})` }} */>{main.content}</div>
-        {/* <div className={styles.right}>
-          <span className={styles.collapse_btn} onClick={toggleRightPanelCollapsed}>
-            <RightOutlined rotate={rightPanelCollapsed ? 180 : 0} />
+        <div className={styles.left}>
+          {main.left}
+          <span className={styles.collapse_btn} onClick={togglePanelCollapsed}>
+            <LeftOutlined rotate={panelCollapsed ? 180 : 0} />
           </span>
-          {main.right}
-        </div> */}
+        </div>
+        <div className={styles.content}>{main.content}</div>
       </div>
       {/* <div className={styles.footer}>{footer}</div> */}
       <span className={styles.exit} onClick={exit}>退出</span>
