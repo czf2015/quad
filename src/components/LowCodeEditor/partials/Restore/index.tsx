3a7f9565daf4476a249ec176f7d197e291e932f9
caizhangfeng <122385930@qq.com>
2022-08-30 23:14:58 +0800

    调整样式

--- src/components/LowCodeEditor/partials/Restore/index.tsx
+++ src/components/LowCodeEditor/partials/Restore/index.tsx
@@ -7,10 +7,10 @@ import styles from './index.module.less'
 export const Restore = ({ visible, prev = 0, next = 0, renew, undo, redo, stage }) => {
   return visible ? (
     <div className={styles.restore}>
-      <Button tip="恢复" onClick={renew} type="text" icon={<LayoutOutlined />} />
-      <Button tip="撤销" disabled={prev == 0} onClick={undo} type="text" icon={<UndoOutlined />} />
-      <Button tip="重做" disabled={next == 0} onClick={redo} type="text" icon={<RedoOutlined />} />
-      <Button tip="暂存" onClick={stage} type="text" icon={<CameraOutlined />} />
+      <Button tip="恢复" onClick={renew} type="text" icon={<LayoutOutlined />} style={{ color: 'var(--quad-white-color)' }} />
+      <Button tip="撤销" disabled={prev == 0} onClick={undo} type="text" icon={<UndoOutlined />} style={{ color: 'var(--quad-white-color)' }} />
+      <Button tip="重做" disabled={next == 0} onClick={redo} type="text" icon={<RedoOutlined />} style={{ color: 'var(--quad-white-color)' }} />
+      <Button tip="暂存" onClick={stage} type="text" icon={<CameraOutlined />} style={{ color: 'var(--quad-white-color)' }} />
     </div>
   ) : null
 }
