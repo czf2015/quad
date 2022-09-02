128331237e19f0cd4ce08fecabc1d1129b18b797
caizhangfeng <122385930@qq.com>
2022-09-02 10:24:07 +0800

    优化交互

--- src/components/LowCodeEditor/partials/DisplayViewer/partials/DragBlock/index.tsx
+++ src/components/LowCodeEditor/partials/DisplayViewer/partials/DragBlock/index.tsx
@@ -24,11 +24,11 @@ export const DragBlock = ({ removeEntity, updateEntity, handleDrop, children, ac
     <div ref={ref} className={`${styles.drag_block} ${editable ? styles.editable : ''} ${entity?.rotate == 0  ? styles.resize : ''}`} style={{ ...entity.style, transform: `rotate(${- entity.rotate}deg)`, ...convertToStyle(entity?.styleConfig) }} {...attrs} onDragStart={handleDragStart('move')} onDrop={onDrop}>
       {children}
       <Mask className={styles.mask} />
-      {['top', 'right', 'bottom', 'left'].map(flag => <div className={styles[`line__${flag}`]} {...attrs} onDragStart={handleDragStart(flag)} key={flag} />)}
-      {['top_left', 'top_right', 'bottom_right', 'bottom_left'].map(flag => <div className={`${styles.circle} ${styles[flag]}`} {...attrs} onDragStart={handleDragStart(flag)} key={flag} />)}
       <Dropdown overlay={<InputNumber style={{ width: 140, textAlign: 'center' }} value={entity.rotate} onChange={handleRotateChange} min={0} max={360} step={5} size="small" addonBefore={<img src="/icons/Angle.svg" width="8px"  height="8px" />} addonAfter="°" />}  placement="bottom">
         <SyncOutlined className={styles.rotate} {...attrs} onDragStart={handleDragStart('rotate')} />
       </Dropdown>
+      {['top', 'right', 'bottom', 'left'].map(flag => <div className={styles[`line__${flag}`]} {...attrs} data-width={entity?.style?.width} data-height={entity?.style?.height} onDragStart={handleDragStart(flag)} key={flag} />)}
+      {['top_left', 'top_right', 'bottom_right', 'bottom_left'].map(flag => <div className={`${styles.circle} ${styles[flag]}`} {...attrs} onDragStart={handleDragStart(flag)} key={flag} />)}
       <Popover content={<BlockStyleConfigPanel {...entity} updateEntity={updateEntity} />} trigger="click">
         <MoreOutlined className={styles.more_btn} />
       </Popover>
